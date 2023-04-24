79f63e17eba4bfda1103431392728a8dd573d620
Tiakowski <tiagosantosrazr@gmail.com>
2023-04-24 20:22:17 -0300

    API

---
+++ API/index.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+var DB = {
+    games: [
+        {
+            id: 1,
+            title:"Valorant",
+            year:"2019",
+            price:"0"
+        },
+        {
+            id: 2,
+            title:"Minecraft",
+            year:"2010",
+            price:"60"
+        },
+        {
+            id: 3,
+            title:"Call of Duty: Modern Warfare",
+            year:"2022",
+            price:"300"
+        }
+    ]
+}
+
+app.get("/games",(req,res) => {
+    res.statusCode = 200;
+    res.json(DB.games);
+});
+
+app.get("/game/:id",(req,res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+
+        var id = parseInt(req.params.id);
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined){
+            res.sendStatus = 200;
+            res.json(game);
+
+        } else {
+            res.sendStatus(404);
+        }
+
+    }
+
+});
+
+app.post("/game",(req,res) => {
+    var {title, price, year} = req.body;
+    DB.games.push({
+        id: 5,
+        title,
+        price,
+        year
+    });
+
+    res.sendStatus(200)
+})
+
+app.delete("/game/:id",(req,res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+
+        var id = parseInt(req.params.id);
+        var index = DB.games.findIndex(g => g.id == id);
+
+        if(index == -1){
+            res.sendStatus(404);
+        } else {
+            DB.games.splice(index,1);
+            res.sendStatus(200);
+        }
+    }
+
+});
+
+app.put("/game/:id",(req,res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+
+        var id = parseInt(req.params.id);
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined){
+            var {title, price, year} = req.body;
+
+            if(title != undefined){
+                game.title = title;
+            }
+
+            if(price != undefined){
+                game.price = price;
+            }
+
+            if(year != undefined){
+                game.year = year;
+            }
+
+            res.sendStatus(200);
+        
+
+        } else {
+            res.sendStatus(404);
+        }
+
+    }
+
+});
+
+
+
+app.listen(8080,() => {
+    console.log("Servidor On-line");
+});
