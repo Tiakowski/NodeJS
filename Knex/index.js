c63a2b12f658e94b3d2361fd677fe523247d07f7
Tiakowski <tiagosantosrazr@gmail.com>
2023-05-02 21:54:26 -0300

    Usando Knex pro Banco de Dados

---
+++ Knex/index.js
@@ -0,0 +1,139 @@
+var database = require("./database");
+
+// Inserir dados:
+
+/*
+var dados = [
+    {
+    nome: "Valorant",
+    preco: 0
+    },
+    {
+    nome: "FIFA 23",
+    preco: 250
+    },
+    {
+    nome: "Fortnite",
+    preco: 0
+    }
+
+
+]
+
+var query = database.insert(dados).into("games").then( data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
+*/
+
+
+//Select
+
+/*
+var query = database.select(["nome","preco"]).table("games").then( data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
+*/
+
+// WHERE
+/*
+database.select()
+    .whereRaw("nome = 'FIFA 23' or preco < 10")
+    .table("games").then( data => {
+        console.log(data)
+    }).catch(err => {
+        console.log(err)
+    });
+
+*/
+
+
+// Query
+/*
+database.raw("SELECT * FROM games").then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+});
+*/
+
+// Delete
+
+/*
+database.where({id: 1}).delete().table("games").then( data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+})
+*/
+
+//Update
+/*
+database.where({nome: "FIFA 23"}).update({preco: 200}).table("games").then(data => {
+    console.log(data);
+}).catch( err => {
+    console.log(err);
+});
+*/
+
+//OrderBy
+/*
+database.select().table("games").orderBy("preco","desc").then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+}); // desc //asc
+*/
+
+
+//Associated inserts
+
+/*
+database.insert({
+    nome: "EA Sports",
+    game_id: 4
+}).table("estudios").then(data =>{
+    console.log(data);
+}).catch(err => {
+    console.log(err)
+});
+*/
+
+
+// Join 1 pra 1
+
+/*
+database.select(["games.*","estudios.nome as estudio"]).table("games").innerJoin("estudios","estudios.game_id","games.id").then( data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err)
+});
+*/
+
+// Iner Join 1 para M (Com os estudios no mesmo array)
+
+/*
+database.select(["games.*","estudios.nome as estudio"]).table("games").innerJoin("estudios","estudios.game_id","games.id").then( data => {
+    var estudiosGames = data;
+    var game = {
+        id: 0,
+        nome: "",
+        estudios: []
+    }
+
+    game.id = data[0].id;
+    game.nome = data[0].nome;
+
+    data.forEach( estudio => {
+        game.estudios.push({nome: estudio.estudio});
+    });
+    console.log(game);
+
+}).catch(err => {
+    console.log(err)
+});
+
+*/
