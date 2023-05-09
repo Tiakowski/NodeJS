3e6f0d8d51adefddbfd375e8f68942dd57ec4295
Tiakowski <tiagosantosrazr@gmail.com>
2023-05-08 21:41:21 -0300

    Começando no VueJS

---
+++ VueJS/clientes/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+      <componente-cliente/>
+      <componente-produto/>
+  </div>
+</template>
+
+<script>
+import ComponenteCliente from './components/ComponenteCliente.vue'
+import ComponenteProduto from './components/ComponenteProduto.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ComponenteCliente,
+    ComponenteProduto
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueJS/clientes/src/components/ComponenteCliente.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input type="text" :value="time">
+        <h1>Atleta:</h1>
+        <h2>Nome: {{nome}}</h2>
+        <h2>Numero: {{numero}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            nome: "Gabriel",
+            numero: 10,
+            time: "Flamengo"
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
