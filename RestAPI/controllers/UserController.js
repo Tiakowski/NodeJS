08c7beef1a0389a41ec4ca5f37f365b20306850d
Tiakowski <tiagosantosrazr@gmail.com>
2023-05-22 21:46:56 -0300

    Edit e Delete

--- RestAPI/controllers/UserController.js
+++ RestAPI/controllers/UserController.js
@@ -1,14 +1,30 @@
 var User = require("../models/User")
 
 class UserController {
-    async index(req, res){}
+    async index(req, res){
+        var users = await User.findAll();
+        res.json(users);
+    }
+
+    async findUser(req, res){
+        var id = req.params.id;
+        var user = await User.findById(id)
+
+        if (user == undefined){
+            res.status(404);
+            res.json({})
+        } else {
+            res.status(200);
+            res.json(user)
+        }
+    }
 
     async create(req, res){
         console.log(req.body)
         var{email, name, password} = req.body;
         if(email == undefined){
             res.status(400);
-            res.json({error: "Email inválido", sytax:"'email': 'email@dominio.com'"})
+            res.json({error: "Email inválido", sytax:"'email': 'email@dominio.com'"});
             return;
         }
 
@@ -25,6 +41,37 @@ class UserController {
 
         res.status(200);
         res.send("Ok");
+    }
+
+    
+    async edit(req, res){
+        var {id, name, role, email} = req.body;
+        var result = await User.update(id, email, name, role)
+        if (result != undefined){
+            if(result.status){
+                res.status(200);
+                res.send("Ok!");
+            } else {
+                res.status(406);
+                res.send(result.err)
+            }
+        } else {
+            res.status(406);
+            res.send(result.err)
+        }
+    }
+    
+    async remove(req, res){
+        var id = req.params.id;
+        var result = await User.delete(id);
+
+        if(result.status){
+            res.status(200);
+            res.send("Ok!")
+        } else {
+            res.status(404);
+            res.send(result.err)
+        }
     }
 }
 
--- RestAPI/models/User.js
+++ RestAPI/models/User.js
@@ -3,6 +3,32 @@ var bcrypt = require("bcrypt");
 
 class User{
 
+    async findAll(){
+        try{
+            var result = await knex.select(['id','email', 'role', 'name']).table("users");
+            return result;
+        }catch(err){
+            console.log(err);
+            return [];
+        }
+        
+    }
+
+    async findById(id){
+        try{
+            var result = await knex.select(['id','email', 'role', 'name']).where({id: id }).table("users");
+            if (result.length > 0){
+                return result;
+            } else {
+                return undefined;
+            }
+            
+        }catch(err){
+            console.log(err);
+            return [];
+        }
+    }
+
     async new(email, password, name){
 
         try{
@@ -30,6 +56,61 @@ class User{
             console.log(err)
             return false;
         }
+        
+    }
+
+    async update(id, email, name, role){
+        var user = await this.findById(id);
+
+        if(user != undefined){
+            var editUser = {};
+            if (email != undefined){
+                if(email != user.email){
+                    var result = await this.findEmail(email) //Retorna True ou False
+                    if (!result){
+                        editUser.email = email;
+                    } else {
+                        return {status: false, err:"Email já cadastrado"}
+                    }
+                }
+            }
+            
+            if(name != undefined){
+                editUser.name = name;
+            }
+            
+            if(role != undefined){
+                editUser.role = role;
+            }
+
+            try {
+                await knex.update(editUser).where({id: id}).table("users");
+                return {status: true}
+            }catch(err){
+                console.log(err)
+                return {status: false, err: err}
+            }
+            
+
+        } else {
+            return {status: false, err:"Usuário não encontrado"}
+        }
+    }
+
+    async delete(id){
+        var user = await this.findById(id);
+        if(user != undefined){
+            try{
+                await knex.delete().where({id: id}).table("users");
+                return {status: true}
+            }catch(err){
+                return {status: false, err: err}
+            }
+            
+        } else {
+            return {status: false, err: "Usuário não encontrado"}
+        }
+    
         
     }
 
